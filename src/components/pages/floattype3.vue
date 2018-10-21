9094eaa863bd25a39b934d083167ee415969e3b0
unizalin <unizalin@gmail.com>
2018-10-22 00:45:46 +0800

    todo list buttom type5 ???

--- src/components/pages/floattype3.vue
+++ src/components/pages/floattype3.vue
@@ -53,6 +53,22 @@
                   <label for="border">邊框顏色</label>
                   <input type="text" class="form-control mt-2" id="border" placeholder="#e6e6e6" maxlength="7" v-model.trim="borderColor" :style="{background:borderColor}">
                 </div>
+                <div class="mt-2">
+                  <label for="inputState">按鈕底色</label>
+                  <div class="btns p2 mt-2 ">
+                    <div class="col mt-2 p-0 df">
+                      <button type="button" v-on:click="changeColor" class="btn gradient gradient-2 " id="gradient-2">Secondary</button>
+                      <button type="button" v-on:click="changeColor" class="btn gradient gradient-3 " id="gradient-3">Success</button>
+                      <button type="button" v-on:click="changeColor" class="btn gradient gradient-4 " id="gradient-4">Danger</button>
+                      <button type="button" v-on:click="changeColor" class="btn gradient gradient-5 " id="gradient-5">Warning</button>
+                    </div>
+                    <div class="col mt-2 p-0">
+                      <button type="button" v-on:click="changeColor" class="btn gradient gradient-6 " id="gradient-6">Info</button>
+                      <button type="button" v-on:click="changeColor" class="btn gradient gradient-7 " id="gradient-7">Light</button>
+                      <button type="button" v-on:click="changeColor" class="btn gradient gradient-8 " id="gradient-8">Dark</button>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
@@ -95,11 +111,17 @@ export default {
     },
     remove(index) {
       this.items.splice(index, 1);
-    }
+    },
+    changeColor (e) {
+      const heading = document.querySelector('BT_COLOUR');
+      const gradient = e.target.id;
+      heading.classList = [];
+      heading.classList.add(gradient);
+  }
   }
 };
 </script>
-<style scoped>
+<style scoped lang="scss">
 * {
   box-sizing: border-box;
 }
@@ -246,6 +268,116 @@ a.X::after {
     #fb2121 100%
   ); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
   filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#b21424', endColorstr='#fb2121',GradientType=0 ); /* IE6-9 */
+}
+
+.gradient-2 {
+  background: #21D4FD;
+  background: linear-gradient(to bottom, #21D4FD 0%, #B721FF 100%);
+  background: -moz-linear-gradient(top, #21D4FD 0%,#B721FF 100%);
+  background: -webkit-linear-gradient(top, #21D4FD 0%,#B721FF  100%);
+  filter: progid:DXImageTransform.Micorsoft.gradient(startColorstr="#21D4FD",endColorstr="#B721FF",GradientType=0);//IE6-9
+  &:hover{
+    background: #B721FF;
+    background: linear-gradient(to bottom, #B721FF 0%, #21D4FD 100%);
+    background: -moz-linear-gradient(top,#B721FF  0%,#21D4FD 100%);
+    background: -webkit-linear-gradient(top, #B721FF 0%, #21D4FD 100%);
+    filter: progid:DXImageTransform.Micorsoft.gradient(startColorstr="#B721FF",endColorstr="#21D4FD",GradientType=0);//IE6-9
+  }
+}
+
+.gradient-3 {
+  background:#FA8BFF;
+  background: linear-gradient(to bottom, #FA8BFF 0%, #2BFF88 100%);
+  background: -moz-linear-gradient(top,#FA8BFF 0%,#2BFF88 100%);
+  background: -webkit-linear-gradient(top,#FA8BFF 0%,#2BFF88 100%);
+  filter: progid:DXImageTransform.Micorsoft.gradient(startColorstr="#FA8BFF",endColorstr="#2BFF88",GradientType=0);//IE6-9
+  &:hover{
+    background:#2BFF88;
+    background: linear-gradient(to bottom, #2BFF88 0%, #FA8BFF 100%);
+    background: -moz-linear-gradient(top,#2BFF88 0%,#FA8BFF 100%);
+    background: -webkit-linear-gradient(top,#2BFF88 0%,#FA8BFF 100%);
+    filter: progid:DXImageTransform.Micorsoft.gradient(startColorstr="#2BFF88",endColorstr="#FA8BFF",GradientType=0);//IE6-9
+  }
+}
+
+.gradient-4 {
+  background: #FF9A8B;
+  background: linear-gradient(to bottom, #FF9A8B 0%, #B721FF 100%);
+  background: -moz-linear-gradient(top, #FF9A8B 0%, #B721FF 100%);
+  background: -webkit-linear-gradient(top, #FF9A8B 0%, #B721FF 100%);
+  filter: progid:DXImageTransform.Micorsoft.gradient(startColorstr="#FF9A8B",endColorstr="#B721FF",GradientType=0);//IE6-9
+  &:hover{
+    background: #FF9A8B;
+    background: linear-gradient(to bottom, #B721FF 0%, #FF9A8B 100%);
+    background: -moz-linear-gradient(top, #B721FF 0%, #FF9A8B 100%);
+    background: -webkit-linear-gradient(top, #B721FF 0%, #FF9A8B 100%);
+    filter: progid:DXImageTransform.Micorsoft.gradient(startColorstr="#B721FF",endColorstr="#FF9A8B",GradientType=0);//IE6-9
+
+  }
+}
+
+.gradient-5 {
+  background: #FFDEE9;
+  background: linear-gradient(to bottom, #FFDEE9 0%, #B5FFFC 100%);
+  background: -moz-linear-gradient(top, #FFDEE9 0%, #B5FFFC 100%);
+  background: -webkit-linear-gradient(top, #FFDEE9 0%, #B5FFFC 100%);
+  filter: progid:DXImageTransform.Micorsoft.gradient(startColorstr="#FFDEE9",endColorstr="#B5FFFC",GradientType=0);//IE6-9
+  &:hover{
+    background: #B5FFFC;
+    background: linear-gradient(to bottom, #B5FFFC 0%, #FFDEE9 100%);
+    background: -moz-linear-gradient(top, #B5FFFC 0%, #FFDEE9 100%);
+    background: -webkit-linear-gradient(top, #B5FFFC 0%, #FFDEE9 100%);
+    filter: progid:DXImageTransform.Micorsoft.gradient(startColorstr="#B5FFFC",endColorstr="#FFDEE9",GradientType=0);//IE6-9
+
+  }
+}
+
+.gradient-6 {
+  background: #3EECAC;
+  background: linear-gradient(to bottom, #3EECAC 0%, #EE74E1 100%);
+  background: -moz-linear-gradient(top, #3EECAC 0%, #EE74E1 100%);
+  background: -webkit-linear-gradient(top, #3EECAC 0%, #EE74E1 100%);
+  filter: progid:DXImageTransform.Micorsoft.gradient(startColorstr="#3EECAC",endColorstr="#EE74E1",GradientType=0);//IE6-9
+  &:hover{
+    background: #EE74E1;
+    background: linear-gradient(to bottom, #EE74E1 0%, #3EECAC 100%);
+    background: -moz-linear-gradient(top, #EE74E1 0%, #3EECAC 100%);
+    background: -webkit-linear-gradient(top, #EE74E1 0%, #3EECAC 100%);
+    filter: progid:DXImageTransform.Micorsoft.gradient(startColorstr="#EE74E1",endColorstr="#3EECAC",GradientType=0);//IE6-9
+
+  }
+}
+
+.gradient-7 {
+  background: #FF3CAC;
+  background: linear-gradient(to bottom, #FF3CAC 0%, #2B86C5 100%);
+  background: -moz-linear-gradient(top, #FF3CAC 0%, #2B86C5 100%);
+  background: -webkit-linear-gradient(top, #FF3CAC 0%, #2B86C5 100%);
+  filter: progid:DXImageTransform.Micorsoft.gradient(startColorstr="#FF3CAC",endColorstr="#2B86C5",GradientType=0);//IE6-9
+  &:hover{
+    background: #2B86C5;
+    background: linear-gradient(to bottom, #2B86C5 0%, #FF3CAC 100%);
+    background: -moz-linear-gradient(top, #2B86C5 0%, #FF3CAC 100%);
+    background: -webkit-linear-gradient(top, #2B86C5 0%, #FF3CAC 100%);
+    filter: progid:DXImageTransform.Micorsoft.gradient(startColorstr="#2B86C5",endColorstr="#FF3CAC",GradientType=0);//IE6-9
+
+  }
+}
+
+.gradient-8 {
+  background: #08AEEA;
+  background: linear-gradient(to bottom, #08AEEA 0%, #2AF598 100%);
+  background: -moz-linear-gradient(top, #08AEEA 0%, #2AF598 100%);
+  background: -webkit-linear-gradient(top, #08AEEA 0%, #2AF598 100%);
+  filter: progid:DXImageTransform.Micorsoft.gradient(startColorstr="#08AEEA",endColorstr="#2AF598 ",GradientType=0);//IE6-9
+  &:hover{
+    background: #2AF598;
+    background: linear-gradient(to bottom, #2AF598 0%, #08AEEA 100%);
+    background: -moz-linear-gradient(top, #2AF598 0%, #08AEEA 100%);
+    background: -webkit-linear-gradient(top, #2AF598 0%, #08AEEA 100%);
+    filter: progid:DXImageTransform.Micorsoft.gradient(startColorstr="#2AF598",endColorstr="#08AEEA ",GradientType=0);//IE6-9
+
+  }
 }
 </style>
 
--- src/components/pages/floattype4.vue
+++ src/components/pages/floattype4.vue
@@ -27,6 +27,22 @@
                     <input type="text" class="form-control mt-2" id="text" placeholder="立即加入" v-model.trim="buttonText">
                   </div>
                 </div>
+                <div class="mt-2">
+                  <label for="inputState">按鈕底色</label>
+                  <div class="btns p2 mt-2 ">
+                    <div class="col mt-2 p-0 df">
+                      <button type="button" v-on:click="changeColor" class="btn gradient gradient-2 " id="gradient-2">Secondary</button>
+                      <button type="button" v-on:click="changeColor" class="btn gradient gradient-3 " id="gradient-3">Success</button>
+                      <button type="button" v-on:click="changeColor" class="btn gradient gradient-4 " id="gradient-4">Danger</button>
+                      <button type="button" v-on:click="changeColor" class="btn gradient gradient-5 " id="gradient-5">Warning</button>
+                    </div>
+                    <div class="col mt-2 p-0">
+                      <button type="button" v-on:click="changeColor" class="btn gradient gradient-6 " id="gradient-6">Info</button>
+                      <button type="button" v-on:click="changeColor" class="btn gradient gradient-7 " id="gradient-7">Light</button>
+                      <button type="button" v-on:click="changeColor" class="btn gradient gradient-8 " id="gradient-8">Dark</button>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
@@ -52,10 +68,18 @@ export default {
     return {
       buttonText: "立即加入"
     };
+  },
+  methods:{
+      changeColor (e) {
+      const heading = document.querySelector('.BT_COLOUR');
+      const gradient = e.target.id;
+      heading.classList = [];
+      heading.classList.add(gradient);
+  }
   }
 };
 </script>
-<style scoped>
+<style scoped lang="scss">
 * {
   /* border: 1px solid black; */
   box-sizing: border-box;
@@ -210,6 +234,116 @@ a.X::after {
     #fb2121 100%
   ); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
   filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#b21424', endColorstr='#fb2121',GradientType=0 ); /* IE6-9 */
+}
+
+.gradient-2 {
+  background: #21D4FD;
+  background: linear-gradient(to bottom, #21D4FD 0%, #B721FF 100%);
+  background: -moz-linear-gradient(top, #21D4FD 0%,#B721FF 100%);
+  background: -webkit-linear-gradient(top, #21D4FD 0%,#B721FF  100%);
+  filter: progid:DXImageTransform.Micorsoft.gradient(startColorstr="#21D4FD",endColorstr="#B721FF",GradientType=0);//IE6-9
+  &:hover{
+    background: #B721FF;
+    background: linear-gradient(to bottom, #B721FF 0%, #21D4FD 100%);
+    background: -moz-linear-gradient(top,#B721FF  0%,#21D4FD 100%);
+    background: -webkit-linear-gradient(top, #B721FF 0%, #21D4FD 100%);
+    filter: progid:DXImageTransform.Micorsoft.gradient(startColorstr="#B721FF",endColorstr="#21D4FD",GradientType=0);//IE6-9
+  }
+}
+
+.gradient-3 {
+  background:#FA8BFF;
+  background: linear-gradient(to bottom, #FA8BFF 0%, #2BFF88 100%);
+  background: -moz-linear-gradient(top,#FA8BFF 0%,#2BFF88 100%);
+  background: -webkit-linear-gradient(top,#FA8BFF 0%,#2BFF88 100%);
+  filter: progid:DXImageTransform.Micorsoft.gradient(startColorstr="#FA8BFF",endColorstr="#2BFF88",GradientType=0);//IE6-9
+  &:hover{
+    background:#2BFF88;
+    background: linear-gradient(to bottom, #2BFF88 0%, #FA8BFF 100%);
+    background: -moz-linear-gradient(top,#2BFF88 0%,#FA8BFF 100%);
+    background: -webkit-linear-gradient(top,#2BFF88 0%,#FA8BFF 100%);
+    filter: progid:DXImageTransform.Micorsoft.gradient(startColorstr="#2BFF88",endColorstr="#FA8BFF",GradientType=0);//IE6-9
+  }
+}
+
+.gradient-4 {
+  background: #FF9A8B;
+  background: linear-gradient(to bottom, #FF9A8B 0%, #B721FF 100%);
+  background: -moz-linear-gradient(top, #FF9A8B 0%, #B721FF 100%);
+  background: -webkit-linear-gradient(top, #FF9A8B 0%, #B721FF 100%);
+  filter: progid:DXImageTransform.Micorsoft.gradient(startColorstr="#FF9A8B",endColorstr="#B721FF",GradientType=0);//IE6-9
+  &:hover{
+    background: #FF9A8B;
+    background: linear-gradient(to bottom, #B721FF 0%, #FF9A8B 100%);
+    background: -moz-linear-gradient(top, #B721FF 0%, #FF9A8B 100%);
+    background: -webkit-linear-gradient(top, #B721FF 0%, #FF9A8B 100%);
+    filter: progid:DXImageTransform.Micorsoft.gradient(startColorstr="#B721FF",endColorstr="#FF9A8B",GradientType=0);//IE6-9
+
+  }
+}
+
+.gradient-5 {
+  background: #FFDEE9;
+  background: linear-gradient(to bottom, #FFDEE9 0%, #B5FFFC 100%);
+  background: -moz-linear-gradient(top, #FFDEE9 0%, #B5FFFC 100%);
+  background: -webkit-linear-gradient(top, #FFDEE9 0%, #B5FFFC 100%);
+  filter: progid:DXImageTransform.Micorsoft.gradient(startColorstr="#FFDEE9",endColorstr="#B5FFFC",GradientType=0);//IE6-9
+  &:hover{
+    background: #B5FFFC;
+    background: linear-gradient(to bottom, #B5FFFC 0%, #FFDEE9 100%);
+    background: -moz-linear-gradient(top, #B5FFFC 0%, #FFDEE9 100%);
+    background: -webkit-linear-gradient(top, #B5FFFC 0%, #FFDEE9 100%);
+    filter: progid:DXImageTransform.Micorsoft.gradient(startColorstr="#B5FFFC",endColorstr="#FFDEE9",GradientType=0);//IE6-9
+
+  }
+}
+
+.gradient-6 {
+  background: #3EECAC;
+  background: linear-gradient(to bottom, #3EECAC 0%, #EE74E1 100%);
+  background: -moz-linear-gradient(top, #3EECAC 0%, #EE74E1 100%);
+  background: -webkit-linear-gradient(top, #3EECAC 0%, #EE74E1 100%);
+  filter: progid:DXImageTransform.Micorsoft.gradient(startColorstr="#3EECAC",endColorstr="#EE74E1",GradientType=0);//IE6-9
+  &:hover{
+    background: #EE74E1;
+    background: linear-gradient(to bottom, #EE74E1 0%, #3EECAC 100%);
+    background: -moz-linear-gradient(top, #EE74E1 0%, #3EECAC 100%);
+    background: -webkit-linear-gradient(top, #EE74E1 0%, #3EECAC 100%);
+    filter: progid:DXImageTransform.Micorsoft.gradient(startColorstr="#EE74E1",endColorstr="#3EECAC",GradientType=0);//IE6-9
+
+  }
+}
+
+.gradient-7 {
+  background: #FF3CAC;
+  background: linear-gradient(to bottom, #FF3CAC 0%, #2B86C5 100%);
+  background: -moz-linear-gradient(top, #FF3CAC 0%, #2B86C5 100%);
+  background: -webkit-linear-gradient(top, #FF3CAC 0%, #2B86C5 100%);
+  filter: progid:DXImageTransform.Micorsoft.gradient(startColorstr="#FF3CAC",endColorstr="#2B86C5",GradientType=0);//IE6-9
+  &:hover{
+    background: #2B86C5;
+    background: linear-gradient(to bottom, #2B86C5 0%, #FF3CAC 100%);
+    background: -moz-linear-gradient(top, #2B86C5 0%, #FF3CAC 100%);
+    background: -webkit-linear-gradient(top, #2B86C5 0%, #FF3CAC 100%);
+    filter: progid:DXImageTransform.Micorsoft.gradient(startColorstr="#2B86C5",endColorstr="#FF3CAC",GradientType=0);//IE6-9
+
+  }
+}
+
+.gradient-8 {
+  background: #08AEEA;
+  background: linear-gradient(to bottom, #08AEEA 0%, #2AF598 100%);
+  background: -moz-linear-gradient(top, #08AEEA 0%, #2AF598 100%);
+  background: -webkit-linear-gradient(top, #08AEEA 0%, #2AF598 100%);
+  filter: progid:DXImageTransform.Micorsoft.gradient(startColorstr="#08AEEA",endColorstr="#2AF598 ",GradientType=0);//IE6-9
+  &:hover{
+    background: #2AF598;
+    background: linear-gradient(to bottom, #2AF598 0%, #08AEEA 100%);
+    background: -moz-linear-gradient(top, #2AF598 0%, #08AEEA 100%);
+    background: -webkit-linear-gradient(top, #2AF598 0%, #08AEEA 100%);
+    filter: progid:DXImageTransform.Micorsoft.gradient(startColorstr="#2AF598",endColorstr="#08AEEA ",GradientType=0);//IE6-9
+
+  }
 }
 </style>
 
